0237f648dafdc12fa13134edeca137a53a03db7e
nahid-hasan <mhnahid1w3r@gmail.com>
2025-11-12 15:59:30 +0600

    Refactored functions and optimized performance before merge.

--- .history/src/axiosPublic_20251112155903.js
+++ .history/src/axiosPublic_20251112155903.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const axiosPublic = axios.create({
-    baseURL: "https://social-events-server-jhuo0cyey-nahid4.vercel.app",
+    baseURL: "https://social-events-server-jhuo0cyey-nahid4.vercel.app/",
 });
 
 export default axiosPublic;
